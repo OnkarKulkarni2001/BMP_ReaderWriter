f3bd3b775691a2f2b7b57fd77959d82001321e17
OnkarKulkarni2001 <onkarkulkarni492001@gmail.com>
2024-11-12 23:32:35 -0500

    Initial commit for static library BMP_ReaderWriter

---
+++ BMP_ReaderWriter/.vshistory/BMP_ReaderWriter.cpp/2024-11-12_23_20_09_226.cpp
@@ -0,0 +1,10 @@
+// BMP_ReaderWriter.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnBMPReaderWriter()
+{
+}
---
+++ BMP_ReaderWriter/.vshistory/BMP_ReaderWriter.h/2024-11-12_23_28_48_039.h
@@ -0,0 +1,29 @@
+// Created by Onkar Parag Kulkarni, Nov 2024
+// Just for portfolio purposes and had some time between some project deadlines that's why xD
+
+#pragma once
+#include <vector>
+
+struct sColor {
+	float r, g, b, a;
+
+	sColor();
+	sColor(float r, float g, float b, float a);
+	~sColor();
+};
+
+class cBMPImage {
+public:
+	cBMPImage(int width, int height);
+	~cBMPImage();
+
+	sColor GetColor(int x, int y);
+	void SetColor(const sColor& color, int x, int y);
+
+	void ExportBMP(const char* path) const;
+
+private:
+	int imageWidth;
+	int imageHeight;
+	std::vector<sColor> vecColors;
+};
